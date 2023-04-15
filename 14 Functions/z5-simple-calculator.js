9e532510f7447426fa65c1a780d0a091b785f027
Atanas Chuchev <127549846+xtxn@users.noreply.github.com>
2023-04-16 01:22:08 +0300

    Lection 14
    All task completed

--- 14 Functions/z5-simple-calculator.js
+++ 14 Functions/z5-simple-calculator.js
@@ -1,20 +1,32 @@
 function simpleCalculator(num1, num2, operator) {
     let result = 0;
+    function multi() {
+        return num1 * num2;
+    }
+    function div() {
+        return num1 / num2;
+    }
+    function add() {
+        return num1 + num2;
+    }
+    function sub() {
+        return num1 - num2;
+    }
+
     switch (operator) {
-        case 'multiply': result = num1 * num2;
+        case 'multiply': result = multi(num1, num2);
             break;
-        case 'divide': result = num1 / num2;
+        case 'divide': result = div(num1, num2)
             break;
-        case 'add': result = num1 + num2;
+        case 'add': result = add(num1, num2);
             break;
-        case 'subtract': result = num1 - num2;
+        case 'subtract': result = sub(num1, num2);
             break;
     }
     console.log(result);
 }
 simpleCalculator(
-    40,
-    8,
-    'divide'
-
+    50,
+    13,
+    'subtract'
 )

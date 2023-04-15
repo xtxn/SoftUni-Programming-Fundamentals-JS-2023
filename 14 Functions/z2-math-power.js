55ff1dae2277e8e1371c50ce4d0608f5c7a3d048
Atanas Chuchev <127549846+xtxn@users.noreply.github.com>
2023-04-15 23:54:48 +0300

    Lection 14

---
+++ 14 Functions/z2-math-power.js
@@ -0,0 +1,7 @@
+function mathPower(num, power) {
+
+    console.log(num ** power);
+}
+mathPower(
+    2, 8
+)
---
+++ 14 Functions/z5-simple-calculator.js
@@ -0,0 +1,20 @@
+function simpleCalculator(num1, num2, operator) {
+    let result = 0;
+    switch (operator) {
+        case 'multiply': result = num1 * num2;
+            break;
+        case 'divide': result = num1 / num2;
+            break;
+        case 'add': result = num1 + num2;
+            break;
+        case 'subtract': result = num1 - num2;
+            break;
+    }
+    console.log(result);
+}
+simpleCalculator(
+    40,
+    8,
+    'divide'
+
+)
